7fc42f56fbfb35da4bec17cb07492a3f744eab9c
cm718 <cm718@icloud.com>
2019-03-09 14:57:38 -0600

    updated styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const H2 = styled.h2`
-    font-size:40px;
-    padding: 50px;
+    font-size:20px;
+    padding: 10px;
     background-color: #333;
     color: white;
     text-align: center;
@@ -11,7 +11,7 @@ const H2 = styled.h2`
 
 const Header = (props) => {
     return(
-        <H2>Don't click an image more than once!</H2>
+        <H2><em>Don't click the same image twice!</em></H2>
     )
 }
 
