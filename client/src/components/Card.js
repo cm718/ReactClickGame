8029d5f29285d0dfe00ca99f1cf528f13fb0dda1
cm718 <cm718@icloud.com>
2019-03-09 14:34:15 -0600

    changed card component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,15 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Image = styled.img`
-    width:200px;
-    height:200px;
+    // width:150px;
+    height:150px;
     object-fit: cover;
     overflow: hidden;
 `
 
 const Card = (props) => {
     return(
-        <Image src={props.image} />
+
+        <div className='col l3 m3 s4 xs4'>
+            <div className='card'>
+                <div className='card-image'>
+                     <Image src={props.image} />
+                </div>            
+            </div>
+        </div>
+        
     )
 }
 
