f20a33db49f2d6d70239ab52fbbbb68d30c6cdf5
cm718 <cm718@icloud.com>
2019-03-09 10:16:04 -0600

    added a Nav and Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import cards from './data.json';
+import Nav from './components/Nav';
+import Header from './components/Header';
 import Card from './components/Card';
 
 //import your cards from json file AS cards
@@ -14,8 +16,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Nav />
-        <Header /> */}
+        <Nav />
+        <Header />
         <div id="cardGrid">
           {this.state.cards.map(card => <Card id={card.id} key={card.id} image={card.image} />)}
         </div>
