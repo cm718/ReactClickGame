712613bade01b56a5f6e2516a3ef1fa454b9ec6d
cm718 <cm718@icloud.com>
2019-03-09 14:57:46 -0600

    added h1 styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,13 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
+const H1 = styled.h1 `
+    font-size: 55px;
+`
 
 const Nav = (props) => {
     return(
-        <h1>Hello!</h1>
+        <H1>Thundercats Memory Game</H1>
     )
 }
 
