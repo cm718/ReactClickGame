ff21d1a7fdd4b2df91c6153d8aee4db3f4748e07
cm718 <cm718@icloud.com>
2019-03-11 17:20:48 -0500

    fixed greeting

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,15 +3,19 @@ import styled from 'styled-components';
 
 const H2 = styled.h2`
     font-size:20px;
+    font-family: hand-written;
     padding: 10px;
     background-color: #333;
     color: white;
     text-align: center;
+    margin: 0;
 `
 
 const Header = (props) => {
+    const greeting = 'Only click each image once!';
+
     return(
-        <H2><em>{props.message}</em></H2>
+        <H2><em>{greeting}</em></H2>
     )
 }
 
