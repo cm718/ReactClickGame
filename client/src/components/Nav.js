f41092bc115a7087778227d6dbc3d9fa91b933a9
cm718 <cm718@icloud.com>
2019-03-11 17:21:11 -0500

    edited styled component for the nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,12 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 const H1 = styled.h1 `
-    font-size: 55px;
+    font-size: 4em;
+    font-family: Permanent Marker;
+    background-color: #333;
+    color: white;
+    padding: 15px;
 `
+const H2 = styled.h2 `
+    font-size: 35px;
+    padding: -20px;
+    font-family: Permanent Marker;
+    background-color: #333;
+    color: white;
+    padding-bottom: 15px;
+    position: relative;
+    top: -20px;
+`
+
+const navStyle = {
+    background: '#333'
+};
 
 const Nav = (props) => {
     return(
-        <H1>Thundercats Memory Game</H1>
+        <div style={navStyle}>
+            <H1>Thundercats</H1>
+            <H2>Memory Game</H2>
+        </div>
     )
 }
 
