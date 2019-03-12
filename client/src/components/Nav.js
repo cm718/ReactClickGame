9b60a027f4609f3722fe805ef28df936d1e973ce
cm718 <cm718@icloud.com>
2019-03-11 22:45:36 -0500

    styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const H1 = styled.h1 `
-    font-size: 4em;
+    font-size: 3em;
     font-family: Permanent Marker;
     background-color: #333;
     color: white;
     padding: 15px;
+    position: top;
+    display: block;
+    position: absolute;
+    top: -50px;
+    width: 100%;
+    margin-bottom: 100px;
 `
 const H2 = styled.h2 `
     font-size: 35px;
@@ -16,14 +23,14 @@ const H2 = styled.h2 `
     color: white;
     padding-bottom: 15px;
     position: relative;
-    top: -20px;
+    top: 35px;
 `
 
 const navStyle = {
-    background: '#333'
+    background: '#333',
 };
 
-const Nav = (props) => {
+const Nav = props => {
     return(
         <div style={navStyle}>
             <H1>Thundercats</H1>
