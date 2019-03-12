7b73456389f8dd5f7af7a53d82c452d4e730735f
cm718 <cm718@icloud.com>
2019-03-11 23:09:08 -0500

    font size increase and margin increase

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const H2 = styled.h2`
-    font-size:20px;
+    font-size:32px;
     font-weight: 700;
     padding: 15px;
     background-color: #333;
     color: white;
     text-align: center;
-    margin-top: 0;
+    margin-top: 50px;
 `
 
 const Header = props => {
