a69ea8784b323adcd4192c0c6f2f11a6381f7adb
cm718 <cm718@icloud.com>
2019-03-11 19:11:58 -0500

    new footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        React Click Game Copyright 2019
+      </footer>
+    )
+  }
+}
+
+export default Footer
